b5442f6996b639288e8b9a5a84e9aa1f656d00a1
Berat-Techstarter <berat.murati@tn.techstarter.de>
2025-04-08 10:54:30 +0200

    print

---
+++ frontend/src/Print.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Print() {
+  return (
+    <div>
+      <h2>Taskliste</h2>
+      <p>Hier könnten die Aufgaben angezeigt werden!</p>
+    </div>
+  );
+}
+
+export default Print;
